0ab8539a0dcaaa6bb797eeaf3c3fd5e3cd545c15
steell911 <petryaev.alexander@ya.ru>
2017-04-06 00:02:53 +0300

    Second commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+//  second commit comment
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+
+    return app.exec();
+}
